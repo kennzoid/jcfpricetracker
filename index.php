d48a4499c2a522f449630ac66a50dc07cb630894
kennzoid <kennzoid@gmail.com>
2014-01-14 14:10:23 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+		include('header.php');
+		echo "<h3>j.crew factory</h3>";
+  		include('simple_html_dom.php');
+?>
+
+<table class="categories">
+  <tr>
+    <td class="catColumn">
+     <ul class="menscolumn">
+      <li class="catColumnTitle">MEN</li>
+  	  <li class="catCell">
+        <a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/mens-clothing/shirts.jsp?iNextCategory=-1&type=jcf_shirt> SHIRTS </a>
+      </li>
+	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/mens-clothing/tees_polos_fleece.jsp?iNextCategory=-1&type=jcf_tee> 
+        TEES, POLOS, FLEECE</a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/mens-clothing/sweaters.jsp?iNextCategory=-1&type=jcf_sweater> SWEATERS </a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/mens-clothing/pants.jsp&type=jcf_pants> PANTS </a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/mens-clothing/jackets_outerwear.jsp?iNextCategory=-1&type=jcf_outerwear> OUTERWEAR </a>
+      </li>
+	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/mens-clothing/suiting.jsp?iNextCategory=-1&type=jcf_suit> SUITING </a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/mens-clothing/ties_pocket_squares.jsp?iNextCategory=-1&type=jcf_tie> TIES </a>
+      </li>
+	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/mens-clothing/accessories.jsp?iNextCategory=-1&type=jcf_accessory> ACCESSORIES </a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/mens-clothing/boxers_sleepwear.jsp?iNextCategory=-1&type=jcf_sleepwear> SLEEPWEAR </a>
+      </li>
+      </ul>
+    </td>
+    
+    <td class="catColumn">
+     <ul class="menscolumn">
+     <li class="catColumnTitle">WOMEN</li>
+  	  <li class="catCell">
+        <a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/sweaters.jsp?iNextCategory=-1&type=jcfw_sweater> SWEATERS </a>
+      </li>
+	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/knits_tees.jsp?iNextCategory=-1&type=jcfw_tee> KNITS & TEES </a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/shirts_tops.jsp?iNextCategory=-1&type=jcfw_shirt> SHIRTS & TOPS </a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/blazers_outerwear.jsp?iNextCategory=-1&type=jcfw_outerwear> OUTERWEAR 
+        </a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/pants.jsp?iNextCategory=-1&type=jcfw_pants> PANTS </a>
+      </li>
+	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/skirts.jsp?iNextCategory=-1&type=jcfw_skirt> SKIRTS </a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/dresses.jsp?iNextCategory=-1&type=jcfw_dress> DRESSES </a>
+      </li>
+	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/shorts.jsp?iNextCategory=-1&type=jcfw_shorts> SHORTS </a>
+      </li>
+      <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/suiting.jsp?iNextCategory=-1&type=jcfw_suit> SUITING </a>
+      </li>
+  	  <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/loungewear.jsp?iNextCategory=-1&type=jcfw_sleepwear> SLEEPWEAR </a>
+      </li>
+      <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/jewelry.jsp?iNextCategory=-1&type=jcfw_jewelery> JEWELERY </a>
+      </li>
+      <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/bags_accessories.jsp?iNextCategory=-1&type=jcfw_accessory> 
+        HANDBAGS & ACCESSORIES </a>
+      </li>
+      <li class="catCell">
+    	<a href=jcrewf_printProducts.php?page=http://factory.jcrew.com/womens-clothing/shoes.jsp?iNextCategory=-1&type=jcfw_shoe> SHOES </a>
+      </li>
+      </ul>
+    </td>
+  </tr>  
+
+
+		
+
+<?php include('footer.php'); ?>
